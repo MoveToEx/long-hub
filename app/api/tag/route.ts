49d869f82b710f15a5333c2070a0161bbdd84bff
MoveToEx <39954725+MoveToEx@users.noreply.github.com>
2023-11-28 22:29:47 +0800

    fix api/tag being a static route

--- app/api/tag/route.ts
+++ app/api/tag/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Tag } from '@/lib/db';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: NextRequest) {
     const tags = await Tag.findAll();
     let result = [];
