8f1aba1f3ada72daf6ec19ae2b8fe6830bbaaca1
MoveToEx <39954725+MoveToEx@users.noreply.github.com>
2023-12-16 20:23:11 +0800

    remove ref on CopiableImage

--- components/CopiableImage.tsx
+++ components/CopiableImage.tsx
@@ -2,42 +2,7 @@
 
 import Image from 'next/image';
 import _ from 'lodash';
-import { useSnackbar, EnqueueSnackbar } from 'notistack';
-import { useRef } from 'react';
-
-async function CopyImage(img: HTMLImageElement | null, enqueueSnackbar: EnqueueSnackbar) {
-    if (!img) {
-        enqueueSnackbar('Uncompressed image is still loading', { variant: 'info' });
-        return;
-    }
-    const write = (blobs: Record<string, Blob>) => {
-        const item = new ClipboardItem(blobs);
-        navigator.clipboard.write([item])
-            .then(() => enqueueSnackbar('Copied to clipboard', { variant: 'success' }))
-            .catch((e) => enqueueSnackbar('Failed when copying: ' + e, { variant: 'error' }));
-    }
-
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
-    if (context === null) throw new Error('unable to get canvas context');
-
-    canvas.width = img.naturalWidth;
-    canvas.height = img.naturalHeight;
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    context.drawImage(img, 0, 0);
-
-    canvas.toBlob(blob => {
-        if (blob === null) throw new Error('unable to convert to png blob');
-
-        write({
-            [blob.type]: blob
-        });
-
-    }, "image/png");
-}
-
+import { useSnackbar } from 'notistack';
 
 export default function CopiableImage({
     src,
@@ -46,11 +11,9 @@ export default function CopiableImage({
     src: string,
     alt: string,
 }) {
-    const image = useRef<HTMLImageElement>(null);
     const { enqueueSnackbar } = useSnackbar();
     return (
         <Image
-            ref={image}
             unoptimized
             crossOrigin="anonymous"
             src={src}
@@ -62,8 +25,8 @@ export default function CopiableImage({
                 height: 'auto',
                 objectFit: 'contain'
             }}
-            onClick={() => {
-                if (image.current === null) return;
+            onClick={(e) => {
+                const img = e.currentTarget;
 
                 const write = (blobs: Record<string, Blob>) => {
                     const item = new ClipboardItem(blobs);
@@ -76,12 +39,12 @@ export default function CopiableImage({
                 const context = canvas.getContext('2d');
                 if (context === null) throw new Error('unable to get canvas context');
 
-                canvas.width = image.current.naturalWidth;
-                canvas.height = image.current.naturalHeight;
+                canvas.width = img.naturalWidth;
+                canvas.height = img.naturalHeight;
                 context.fillStyle = 'white';
                 context.fillRect(0, 0, canvas.width, canvas.height);
 
-                context.drawImage(image.current, 0, 0);
+                context.drawImage(img, 0, 0);
 
                 canvas.toBlob(blob => {
                     if (blob === null) throw new Error('unable to convert to png blob');
