70dc2027946e075ab313dcd1f4fe9963ddae27d9
MoveToEx <39954725+MoveToEx@users.noreply.github.com>
2024-04-26 15:17:05 +0800

    feat: Rewrite tags page using client component

--- app/tag/page.tsx
+++ app/tag/page.tsx
@@ -1,47 +1,70 @@
+'use client';
+
 import Badge from '@mui/material/Badge';
 import Chip from '@mui/material/Chip';
 import TagIcon from '@mui/icons-material/Tag';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from 'next/link';
-import { prisma } from '@/lib/db';
-import { Metadata } from 'next';
+import CircularProgress from '@mui/material/CircularProgress';
+import Modal from '@mui/material/Modal';
+import { useState, useEffect } from 'react';
+import { useSnackbar } from 'notistack';
 
-export const dynamic = 'force-dynamic';
 
-export const metadata: Metadata = {
-    title: 'Tag'
+interface Tag {
+    id: number,
+    name: string,
+    count: number
 };
 
-export default async function Tags() {
-    const tags = await prisma.tag.findMany({
-        select: {
-            _count: {
-                select: {
-                    posts: true
+export default function Tags() {
+    const [tags, setTags] = useState<Tag[]>([]);
+    const { enqueueSnackbar } = useSnackbar();
+
+    useEffect(() => {
+        fetch('/api/post/tag')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(response.statusText);
                 }
-            },
-            name: true
-        }
-    });
+                return response.json();
+            })
+            .then(data => {
+                setTags(data);
+            })
+            .catch(reason => {
+                enqueueSnackbar(reason, { variant: 'error' });
+            });
+    }, [enqueueSnackbar]);
 
     return (
-        <Box sx={{ mt: '12px' }}>
-            {
-                tags.map(tag => (
-                    <Badge
-                        badgeContent={tag._count.posts}
-                        key={tag.name}
-                        color="primary"
-                        sx={{
-                            m: 1
-                        }}>
-                        <Link href={"/tag/" + tag.name} prefetch={false}>
-                            <Chip label={<Typography>{tag.name}</Typography>} icon={<TagIcon />} />
-                        </Link>
-                    </Badge>
-                ))
+        <>
+            {tags.length == 0 &&
+                <CircularProgress autoFocus={false} sx={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                }} />
             }
-        </Box>
+            <Box sx={{ mt: '12px' }}>
+                {
+                    tags.map(tag => (
+                        <Badge
+                            badgeContent={tag.count}
+                            key={tag.name}
+                            color="primary"
+                            sx={{
+                                m: 1
+                            }}>
+                            <Link href={"/tag/" + tag.name}>
+                                <Chip label={<Typography>{tag.name}</Typography>} icon={<TagIcon />} />
+                            </Link>
+                        </Badge>
+                    ))
+                }
+            </Box>
+        </>
     )
 }
