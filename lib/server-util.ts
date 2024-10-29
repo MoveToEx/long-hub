d5e3b5aec4b99ceb528b4e5905beef0766e602f0
MoveToEx <39954725+MoveToEx@users.noreply.github.com>
2024-10-29 18:35:13 +0800

    misc: Remove unused code

--- lib/server-util.ts
+++ lib/server-util.ts
@@ -1,24 +1,5 @@
-import { IronSession, getIronSession } from "iron-session";
-import { Session } from "@/lib/session";
-import { NextRequest, NextResponse } from "next/server";
-import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
 import { ZodError } from 'zod';
 import _ from 'lodash';
-
-export const cookieSettings = {
-    password: process.env['COOKIE_SECRET'] as string,
-    cookieName: process.env['COOKIE_NAME'] as string,
-    ttl: 60 * 24 * 60 * 60,
-}
-
-export function formatZodError(error: ZodError): string {
-    const data = error.flatten();
-    const msg = _.concat(
-        data.formErrors,
-        _.toPairs(data.fieldErrors).map(([field, msg]) => `Error parsing ${field}: ${msg}`)
-    );
-    return msg.join('\n');
-}
 
 export const responses = {
     forbidden() {
