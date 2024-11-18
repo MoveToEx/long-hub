274bd40a507a43a7a9c1999740633e742668c81e
MoveToEx <39954725+MoveToEx@users.noreply.github.com>
2024-11-18 10:53:04 +0800

    misc: Remove search selector icon

--- app/post/search/components.tsx
+++ app/post/search/components.tsx
@@ -11,33 +11,6 @@ import FingerprintIcon from '@mui/icons-material/Fingerprint';
 
 import { useTags } from '@/app/context';
 
-function getLabel(s: string) {
-    if (s.startsWith('+') || s.startsWith('-')) {
-        return {
-            label: _.trimStart(s, '+'),
-            avatar: <TagIcon fontSize='small' />
-        };
-    }
-    else if (s.startsWith('@')) {
-        return {
-            label: _.trimStart(s, '@'),
-            avatar: <FingerprintIcon fontSize='small' />
-        };
-    }
-    // else if (s.startsWith('<') || s.startsWith('>') || s.startsWith('=') || s.startsWith('!=')) {
-    //     return {
-    //         label: s,
-    //         avatar: <></>
-    //     }
-    // }
-    else {
-        return {
-            label: s,
-            avatar: <></>
-        }
-    }
-}
-
 export function SearchInput({
     value,
     onChange
@@ -48,51 +21,51 @@ export function SearchInput({
     const { data, isLoading } = useTags();
 
     return (
-        <>
-            <Autocomplete
-                multiple
-                freeSolo
-                value={value}
-                fullWidth
-                sx={{ mt: 2 }}
-                options={data?.map(val => val.name) ?? []}
-                renderTags={(value: string[], getTagProps) =>
-                    value.map((option: string, index: number) => (
-                        <Chip
-                            variant="outlined"
-                            {...getLabel(option)}
-                            {...getTagProps({ index })}
-                            key={index} />
-                    ))
+        <Autocomplete
+            multiple
+            freeSolo
+            value={value}
+            fullWidth
+            sx={{ mt: 2 }}
+            options={data?.map(val => val.name) ?? []}
+            renderTags={(value: string[], getTagProps) =>
+                value.map((option: string, index: number) => (
+                    <Chip
+                        variant="outlined"
+                        label={option}
+                        {...getTagProps({ index })}
+                        key={index} />
+                ))
+            }
+            renderOption={(props, option) => {
+                return <li {...props} key={option}>{option}</li>;
+            }}
+            filterOptions={(options, { inputValue }) => {
+                if (inputValue.startsWith('+') || inputValue.startsWith('-')) {
+                    return options
+                        .filter(x => x.startsWith(_.trimStart(inputValue, '+-')))
+                        .map(x => inputValue[0] + x);
                 }
-                renderOption={(props, option) => {
-                    return <li {...props} key={option}>{option}</li>;
-                }}
-                filterOptions={(options, { inputValue }) => {
-                    if (inputValue.startsWith('+') || inputValue.startsWith('-')) {
-                        return options
-                            .filter(x => x.startsWith(_.trimStart(inputValue, '+-')))
-                            .map(x => inputValue[0] + x);
-                    }
-                    else {
-                        return [];
-                    }
-                }}
-                onChange={onChange}
-                renderInput={
-                    (params) => (
-                        <TextField
-                            {...params}
-                            sx={{ m: '16px 0 16px 0' }}
-                            label="Search"
-                            variant="outlined"
-                            InputProps={{
+                else {
+                    return [];
+                }
+            }}
+            onChange={onChange}
+            renderInput={
+                (params) => (
+                    <TextField
+                        {...params}
+                        sx={{ m: '16px 0 16px 0' }}
+                        label="Search"
+                        variant="outlined"
+                        slotProps={{
+                            input: {
                                 ...params.InputProps,
                                 endAdornment: isLoading ? <CircularProgress size={20} /> : <></>
-                            }} />
-                    )
-                }
-            />
-        </>
+                            }
+                        }}/>
+                )
+            }
+        />
     )
 }
