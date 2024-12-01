da7f614c8c948a11f22a473044c5b51cc18186c8
MoveToEx <39954725+MoveToEx@users.noreply.github.com>
2024-12-01 14:15:06 +0800

    misc: Remove aggr

---
+++ prisma/migrations/20241201061426_remove_aggr/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `aggr` on the `post` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `post` DROP COLUMN `aggr`;
