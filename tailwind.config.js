e1aad973816f0ce78de600ec9fdb54aa6048ee9f
MoveToEx <39954725+MoveToEx@users.noreply.github.com>
2024-12-08 18:04:19 +0800

    refactor: Add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+export default config;
